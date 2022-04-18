280aa8cf21c1d8ca0bc7d9ff2e14ec4e04fd781f
unknown <alarafath71@gmail.com>
2022-04-18 12:31:35 +0600

    when user loged in then show  sign out

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,14 @@
+import { getAuth } from '@firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import app from '../../firebase.init';
 import './Header.css';
 
+const auth = getAuth(app)
+
 const Header = () => {
+    const [user] = useAuthState(auth)
     return (
         <div className="header items-center mt-7">
             <h2 className="wrytica w-100 text-cyan-800 font-semibold text-4xl font-black">Wrytica.</h2>
@@ -11,7 +17,14 @@ const Header = () => {
                 <Link to="/services">Services</Link>
                 <Link to="/blogs">Blogs</Link>
                 <Link to="/about">About</Link>
-                <Link to="/login">Login</Link>
+                {
+                    user?.uid
+                     ? 
+                     <button className="signOut-btn">Sign out</button> 
+                    : 
+                    <Link to="/login">Login</Link>
+                    
+                }
                 <Link to="*"></Link>
             </div>
         </div>
